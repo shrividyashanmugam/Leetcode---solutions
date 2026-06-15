c4fe157d79c9798c29e40ad4602c26a1a601dca0
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-06-15 12:13:50 +0530

    Time: 1 ms (98.49%), Space: 47.2 MB (77.11%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,19 +1,28 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int leftSum = 0;
-        int totalSum = 0;
-        for(int num : nums){
-            totalSum += num;
+        int n = nums.length;
+        int[] sumLeft = new int[n];
+        int[] sumRight = new int[n];
+        
+        // Hint 1: Fill sumLeft array (moving forward)
+        // sumLeft[0] is automatically 0
+        for (int i = 1; i < n; i++) {
+            sumLeft[i] = sumLeft[i - 1] + nums[i - 1];
         }
-        for(int i =0;i< nums.length;i++){
-                int rigthSum = totalSum - leftSum - nums[i];
-                 
-
-                 if(leftSum == rigthSum){
-                    return i;
-                 }
-                 leftSum += nums[i];
+        
+        // Hint 2: Fill sumRight array (moving backward)
+        // sumRight[n-1] is automatically 0
+        for (int i = n - 2; i >= 0; i--) {
+            sumRight[i] = sumRight[i + 1] + nums[i + 1];
         }
+        
+        // Hint 3: Compare both arrays for each index i
+        for (int i = 0; i < n; i++) {
+            if (sumLeft[i] == sumRight[i]) {
+                return i; // Return the leftmost index immediately
+            }
+        }
+        
         return -1;
     }
 }
