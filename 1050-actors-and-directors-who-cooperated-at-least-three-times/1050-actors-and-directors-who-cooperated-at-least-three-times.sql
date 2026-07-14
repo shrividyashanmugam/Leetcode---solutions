d1080165f64f134e38a779805e3b08b335a69d4b
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-14 22:27:04 +0530

    Time: 442 ms (13.43%), Space: 0B (100%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,4 +1,6 @@
-select actor_id,director_id
-from ActorDirector 
-group by actor_id,director_id
-Having count(timestamp)>=3;
+# Write your MySQL query statement below
+
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(*) >=3
