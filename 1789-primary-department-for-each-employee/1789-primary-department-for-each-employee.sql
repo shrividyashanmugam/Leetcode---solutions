d2de0add144820ca846820431506572bf315e499
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-17 14:52:13 +0530

    Time: 938 ms (5%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,22 +1,14 @@
-# Write your MySQL query statement below
--- Case 1: Employees who have explicitly decided on their primary department
-SELECT 
-    employee_id, 
+select
+    employee_id,
     department_id
-FROM 
-    Employee
-WHERE 
-    primary_flag = 'Y'
+from Employee
+where primary_flag = "Y"
 
 UNION
 
--- Case 2: Employees who belong to only one department
-SELECT 
-    employee_id, 
+select 
+    employee_id,
     department_id
-FROM 
-    Employee
-GROUP BY 
-    employee_id
-HAVING 
-    COUNT(department_id) = 1;
+from Employee
+group by employee_id
+having count(employee_id) = 1;
