163a57b0f1ab68af8787e0cf4ed262b9f468c6e6
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-17 15:15:40 +0530

    Time: 29 ms (87.5%), Space: 102.4 MB (53.13%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -1,65 +1,46 @@
+import java.util.Arrays;
 class Solution {
     public int[] gcdValues(int[] nums, long[] queries) {
         int maxVal = 0;
         for (int num : nums) {
             maxVal = Math.max(maxVal, num);
         }
-        
-        // Count frequencies of each number
-        long[] cnt = new long[maxVal + 1];
+        int[] counts = new int[maxVal + 1];
         for (int num : nums) {
-            cnt[num]++;
+            counts[num]++;
         }
-        
-        // Count how many elements are multiples of each i
-        long[] multiples = new long[maxVal + 1];
-        for (int i = 1; i <= maxVal; i++) {
-            for (int j = i; j <= maxVal; j += i) {
-                multiples[i] += cnt[j];
+        long[] gcdCounts = new long[maxVal + 1];
+        for (int g = maxVal; g >= 1; g--) {
+            long totalMultiples = 0;
+            for (int multiple = g; multiple <= maxVal; multiple += g) {
+                totalMultiples += counts[multiple];
             }
-        }
-        
-        // Count pairs with exact GCD = i using backward Inclusion-Exclusion DP
-        long[] gcdCount = new long[maxVal + 1];
-        for (int i = maxVal; i >= 1; i--) {
-            long pairs = (multiples[i] * (multiples[i] - 1)) / 2;
-            for (int j = 2 * i; j <= maxVal; j += i) {
-                pairs -= gcdCount[j];
+            long totalPairs = (totalMultiples * (totalMultiples - 1)) / 2;
+            for (int multiple = 2 * g; multiple <= maxVal; multiple += g) {
+                totalPairs -= gcdCounts[multiple];
             }
-            gcdCount[i] = pairs;
+
+            gcdCounts[g] = totalPairs;
         }
-        
-        // Compute prefix sums of GCD frequencies
         long[] prefixSums = new long[maxVal + 1];
         for (int i = 1; i <= maxVal; i++) {
-            prefixSums[i] = prefixSums[i - 1] + gcdCount[i];
+            prefixSums[i] = prefixSums[i - 1] + gcdCounts[i];
         }
-        
-        // Answer each query using binary search
-        int[] ans = new int[queries.length];
-        for (int k = 0; k < queries.length; k++) {
-            long q = queries[k];
-            ans[k] = upperBinarySearch(prefixSums, q);
-        }
-        
-        return ans;
-    }
-    
-    // Finds the first index where prefixSums[index] > target
-    private int upperBinarySearch(long[] prefixSums, long target) {
-        int low = 1;
-        int high = prefixSums.length - 1;
-        int ans = high;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (prefixSums[mid] > target) {
-                ans = mid;
-                high = mid - 1; // Look for a smaller index
-            } else {
-                low = mid + 1;
+        int[] answer = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            long targetIndex = queries[i];
+            int low = 1, high = maxVal, res = maxVal;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (prefixSums[mid] > targetIndex) {
+                    res = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             }
+            answer[i] = res;
         }
-        return ans;
+        return answer;
     }
 }
