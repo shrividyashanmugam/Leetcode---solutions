e402256cfdfad725ad93f4f429ffab356c6cc045
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-31 09:18:22 +0530

    Time: 2 ms (27.02%), Space: 42.3 MB (93.37%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        
+        for (int i = 0; i <= rowIndex; i++) {
+            row.add(1); // Add an element for the new row size
+            
+            // Update values from right to left
+            for (int j = i - 1; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+        }
+        
+        return row;
+    }
+}
